da61da1a16946c769dba2f630091748995d0c072
Mohamed <ed_alh@icloud.com>
2023-06-16 09:04:26 -0700

    4-print_alphabt.c added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Initial alphabet aka "a"
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' | a != 'q')
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
