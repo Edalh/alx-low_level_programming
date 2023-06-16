4fcd7ba971b65c91ebc4c98d88cc9e011e23c1a0
Mohamed <ed_alh@icloud.com>
2023-06-16 09:47:42 -0700

    5-print_numbers.c added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * for loop
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 9; n++)
+		putchar(n);
+	putchar('\n');
+}
