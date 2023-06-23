e0e6397885db43bc74f3acaeff415a623c5d876a
Mohamed <ed_alh@icloud.com>
2023-06-22 17:21:42 -0700

    9-fizz_buzz.c updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 			printf("FizzBuzz");
@@ -19,7 +19,7 @@ int main(void)
 		else
 			printf("%i", n);
 
-		if (n < 99)
+		if (n < 100)
 			printf(" ");
 		else
 			printf("\n");
