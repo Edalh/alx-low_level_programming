a57e23ba66a638356a299da206ebd2f9a24e0f7c
Mohamed <ed_alh@icloud.com>
2023-06-29 17:55:57 -0700

    7-leet.c added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ * Return: the resulting s
+ */
+char *leet(char *s)
+{
+	int i, k;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			if (s[i] == a[k])
+			{
+				s[i] = b[k];
+			}
+		}
+	}
+
+	return (s);
+}
